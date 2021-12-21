ac1ae7cb2fbca3af04e4a1b530968fdd40469616
lama <david3951445@gmail.com>
2021-12-21 19:27:05 +0800

    add 3sum

---
+++ 15. 3Sum/3Sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int function() {
+        
+        return 0;
+    }
+};
+
+int main() {
+    Solution sol;
+    cout << sol.function() << endl;
+}
